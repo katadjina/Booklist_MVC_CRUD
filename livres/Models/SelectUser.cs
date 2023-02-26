0ddfa5d460d7c540d6ad5a59a6b99a321644c6a9
katadjina <94002120+katadjina@users.noreply.github.com>
2023-02-26 19:03:21 +0100

    Add files via upload

---
+++ livres/Models/SelectUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace livres.Models
+{
+    public class SelectUser
+    {
+        
+            public List<User> Choix { get; set; }
+           public string ChoixSelectionne { get; set; }
+        
+    }
+}
+
---
+++ livres/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace livres.Models
+{
+    public class User
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+      
+    }
+}
+
