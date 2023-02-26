b68e455a384cdff97130773051cbd5b42e1a1ace
katadjina <94002120+katadjina@users.noreply.github.com>
2023-02-26 19:02:45 +0100

    Add files via upload

---
+++ livres/Controllers/AuthorController.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using livres.FakeDb;
+using livres.Models;
+
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace livres.Controllers
+{
+    public class AuthorController : Controller
+    {
+
+
+       
+
+
+        // GET: /<controller>/
+        //index page will show all the authors
+        public IActionResult Index()
+        {
+
+
+            //testing viewbag:
+            //string by default but can be casted
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good morning" : "Good afternoon";
+
+            //you need to send as parameter the whole list
+            //so the list will be rendered
+            return View(AuthorsDb.GetAll());
+            
+
+
+
+         
+        }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+
+        //if form data is submitted
+        [HttpPost]     
+        public IActionResult Create(Author form)
+        {
+            bool exists = false;
+            if (ModelState.IsValid)
+            {
+                Author newA = new Author
+                {
+                    Name = form.Name,
+                    BirthDate = form.BirthDate
+                   
+                };
+
+                //check if the author already exists to avoid duplicates
+                //doesnt work well
+                
+                foreach (Author a in AuthorsDb.authors)
+                {
+                    if (newA.Name == a.Name)
+                    {
+                        exists = true;
+                       // Console.WriteLine(newA.Name);
+                    }
+                    else
+                    {
+                        exists = false;
+                    }
+                }
+
+
+                if (exists == false)
+                {
+                    AuthorsDb.Add(newA);
+                    return RedirectToAction("index");
+                }
+                else
+                {
+                   
+                    return RedirectToAction("authorExists");
+                }
+
+
+
+            }
+
+            else
+            {
+                //not sure why form is passed as param
+                return View(form);
+            }
+        }
+
+
+        public IActionResult Details(int id)
+        {
+            //like this we can have prefilled forms to see what data we are editing
+            Author a = AuthorsDb.GetById(id);
+            return View(a);
+        }
+
+
+        public IActionResult AuthorExists()
+        {
+            return View();
+        }
+
+
+
+
+        //2 updates for viewing and retrieving form data
+
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            //like this we can have prefilled forms to see what data we are editing
+            //Author a = AuthorsDb.GetById(id);
+            //return View(a);
+            return View(AuthorsDb.GetById(id));
+        }
+
+
+        //not working -> new is added instead//idk how to get old author id so when Update method is called -> 
+        [HttpPost]
+
+        //this method receives the id of tha author that was clicked on and takes data
+        //from the form to create new object --> next this object is added to fakeDB loist
+
+        public IActionResult Update(Author form, int id)
+        {
+            if(ModelState.IsValid)
+            {
+                Author updatedA = new Author
+                {
+                    Name = form.Name,
+                    BirthDate = form.BirthDate
+                   //DateNaissance = new DateTime(form.DateNaissance)
+                };              
+               
+                 AuthorsDb.Update(id, updatedA);
+                 return RedirectToAction("index");
+             }
+            
+
+
+            else
+            {
+                //why form is passed as param
+                return View(form);
+            }
+
+
+            
+        }
+
+
+
+
+
+        public IActionResult Delete(int id)
+        {
+            AuthorsDb.Delete(id);
+            return RedirectToAction("index");
+
+        }
+
+    }
+
+}
+
+
---
+++ livres/Controllers/UserController.cs
@@ -0,0 +1,119 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using livres.Models;
+//using Microsoft.AspNetCore.Mvc;
+
+//// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+//namespace livres.Controllers
+//{
+//    public class UserController : Controller
+//    {
+
+
+//        private static List<User> _users = new List<User>
+//                {
+//                    new User {Prenom = "Sara", Nom = "Pehlivan"},
+//                    new User {Prenom = "Allessandra", Nom = "Rafaele"},
+//                    new User{Prenom = "Eleonore", Nom = "Stultjens", },
+//                    new User {Prenom = "Coline", Nom = "Ducourtieux"},
+                    
+//                };
+//        // GET: /<controller>/
+//        public IActionResult Index()
+//        {
+//            IEnumerable<StudentListItem> model = _students.Select(s => new StudentListItem() { Nom = s.Nom, Prenom = s.Prenom, Email = s.Email });
+//            return View();
+//        }
+
+
+//        // GET: StudentController/Details/5
+//        public ActionResult Details(string id)
+//        {
+//            Student stu = _students.SingleOrDefault(s => s.Email == id);
+//            StudentDetails model = new StudentDetails() { Nom = stu.Nom, Prenom = stu.Prenom, DateNaissance = stu.DateNaissance, Email = stu.Email };
+//            return View(model);
+//        }
+
+//        // GET: StudentController/Create
+//        public ActionResult Create()
+//        {
+//            return View();
+//        }
+
+//        // POST: StudentController/Create
+//        [HttpPost]
+//        [ValidateAntiForgeryToken]
+//        public ActionResult Create(StudentCreateForm collection)
+//        {
+//            if (!ModelState.IsValid) return View();
+//            else
+//            {
+//                collection.Email = $"{collection.Prenom}.{collection.Nom}@interface3.be";
+//                _students.Add(new Student()
+//                {
+//                    Nom = collection.Nom,
+//                    Prenom = collection.Prenom,
+//                    DateNaissance = collection.DateNaissance,
+//                    Email = collection.Email
+//                });
+//                return RedirectToAction(nameof(Index));
+//            }
+//        }
+
+//        // GET: StudentController/Edit/5
+//        public ActionResult Edit(string id)
+//        {
+//            Student stu = _students.SingleOrDefault(s => s.Email == id);
+//            StudentEditForm model = new StudentEditForm()
+//            {
+//                Nom = stu.Nom,
+//                Prenom = stu.Prenom,
+//                DateNaissance = stu.DateNaissance,
+//                Email = stu.Email
+//            };
+//            return View(model);
+//        }
+
+//        // POST: StudentController/Edit/5
+//        [HttpPost]
+//        [ValidateAntiForgeryToken]
+//        public ActionResult Edit(string id, StudentEditForm collection)
+//        {
+//            if (!ModelState.IsValid) return View();
+//            else
+//            {
+//                Student stu = _students.SingleOrDefault(s => s.Email == id);
+//                stu.Nom = collection.Nom;
+//                stu.Prenom = collection.Prenom;
+//                stu.DateNaissance = collection.DateNaissance;
+//                return RedirectToAction(nameof(Index));
+//            }
+//        }
+
+//        // GET: StudentController/Delete/5
+//        public ActionResult Delete(string id)
+//        {
+//            Student stu = _students.SingleOrDefault(s => s.Email == id);
+//            StudentDelete model = new StudentDelete()
+//            {
+//                Nom = stu.Nom,
+//                Prenom = stu.Prenom,
+//                Email = stu.Email
+//            };
+//            return View(model);
+//        }
+
+//        // POST: StudentController/Delete/5
+//        [HttpPost]
+//        [ValidateAntiForgeryToken]
+//        public ActionResult Delete(string id, StudentDelete collection)
+//        {
+//            _students.Remove(_students.SingleOrDefault(s => s.Email == id));
+//            return RedirectToAction(nameof(Index));
+//        }
+//    }
+//}
+
